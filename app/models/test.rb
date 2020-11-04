914c92e4fc04e3eee49adf21e3c88c692aaafcfe
yerlanmad <studyworld.m@gmail.com>
2020-11-04 08:14:21 +0600

    added dependencies, joins with associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
-  has_many :questions
-  has_many :test_histories
+  has_many :questions, dependent: :destroy
+  has_many :test_histories, dependent: :destroy
   has_many :users, through: :test_histories
   
   def self.titles(category)
-    joins("JOIN categories ON tests.category_id = categories.id")
-      .where(categories: { category: category })
+    joins(:category).where(categories: { category: category })
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :test_histories
   has_many :tests, through: :test_histories
-  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id",
+    dependent: :destroy
   
   def passed_tests_by_level(level)
-    Test.joins("JOIN test_histories ON test_histories.test_id = tests.id")
+    Test.joins(:test_histories)
       .where(level: level, test_histories: { user_id: self.id })
       .distinct
   end
