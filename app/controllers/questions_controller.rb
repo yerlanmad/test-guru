7027fdae4860758e5dab7c3f44e130ad98a9f681
yerlanmad <studyworld.m@gmail.com>
2020-11-29 14:16:54 +0600

    corrections after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: :index
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
@@ -8,23 +9,25 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
     render plain: @question.inspect
   end
 
   def new
-    #
+    @question = Question.new
   end
 
   def create
-    @question = Question.create!(question_params)
-    render plain: @question.inspect
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
-
     render inline: "<p>Question was deleted</p>"
   end
 
@@ -32,6 +35,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
