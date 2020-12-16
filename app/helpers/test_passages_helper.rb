880ca94d1b40b3a8787add4e5d39770334250232
yerlanmad <studyworld.m@gmail.com>
2020-12-16 21:57:19 +0600

    last corrections

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,7 +1,7 @@
 module TestPassagesHelper
   RESULT = { failed: { style: :notice_red, message: "You didn't pass the test" }, passed: { style: :notice, message: 'Test was successfully passed' } }.freeze
 
-  def result(test_passage)
-    test_passage.is_passed? ? RESULT[:passed] : RESULT[:failed]
+  def result(is_passed)
+    is_passed ? RESULT[:passed] : RESULT[:failed]
   end
 end
