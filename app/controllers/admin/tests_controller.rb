d58747e7cb0e5b6570c259542c31da18e41e2887
yerlanmad <studyworld.m@gmail.com>
2021-01-12 20:51:50 +0600

    part11 hw

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -22,15 +22,15 @@ class Admin::TestsController < Admin::BaseController
     @test = Test.new(test_params)
 
     if @test.save
-      redirect_to @test
+      redirect_to admin_test_path(@test)
     else
       render :new
     end
   end
 
   def update
-    if @test.update(test_params)
-      redirect_to @test
+    if @test.update(test_params.permit(:title, :level, :category_id))
+      redirect_to admin_test_path(@test)
     else
       render :edit
     end
@@ -38,7 +38,7 @@ class Admin::TestsController < Admin::BaseController
 
   def destroy
     @test.destroy
-    redirect_to tests_path
+    redirect_to admin_tests_path
   end
 
   def start
@@ -53,7 +53,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 
   def rescue_with_test_not_found(exception)
