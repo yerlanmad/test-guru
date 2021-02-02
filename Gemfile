928df80909a06e58b88b5e652bd6cc2d0b53be0c
yerlanmad <studyworld.m@gmail.com>
2021-02-02 12:21:04 +0600

    corrections after review

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise', '~> 4.7', '>= 4.7.3'
 
 # LOCALIZATION
-gem 'rails-i18n', '~> 6.0'
+# gem 'rails-i18n', '~> 6.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  before_action :switch_locale
 
   def default_url_options
     I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
@@ -15,8 +15,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def switch_locale(&action)
-    locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
-    I18n.with_locale(locale, &action)
+  def switch_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
