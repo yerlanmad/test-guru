4c9822b4b1b1938283159d585cdc295f9b6e141e
yerlanmad <studyworld.m@gmail.com>
2021-06-13 14:12:12 +0600

    correct migrations

--- db/migrate/20201010035104_create_categories.rb
+++ db/migrate/20201010035104_create_categories.rb
@@ -1,9 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
-  def change
-    create_table :categories do |t|
-      t.string :category
+  # def change
+  #   create_table :categories do |t|
+  #     t.string :category
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
