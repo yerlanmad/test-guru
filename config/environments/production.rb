5752d8fc4df4edde4a903f206e5c823f3d060344
yerlanmad <studyworld.m@gmail.com>
2021-06-15 11:14:49 +0600

    change stmp to mail.ru

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'mad-guru.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.yandex.ru',
+    address: 'smtp.mail.ru',
     port: 465,
     user_name: ENV.fetch('SMTP_USERNAME'),
     password: ENV.fetch('SMTP_PASSWORD'),
