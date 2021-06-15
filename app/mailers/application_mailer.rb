aebe0a1c05ad06444dd115f453fe0cdc0a01c9cc
yerlanmad <studyworld.m@gmail.com>
2021-06-15 09:07:59 +0600

    change to smpt.yandex.ru

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: %{"TestGuru" <mail@testguru.com>}
+  default from: %{"TestGuru" <yerlan.madiyarov@yandex.ru>}
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'mad-guru.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
+    address: 'smtp.yandex.ru',
+    port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    tls: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
