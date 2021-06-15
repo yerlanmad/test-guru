0df25894c0921d4d704f2bb880368661aef6fb34
yerlanmad <studyworld.m@gmail.com>
2021-06-15 09:29:02 +0600

    mail sendet to .env

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: %{"TestGuru" <yerlan.madiyarov@yandex.ru>}
+  default from: %{"TestGuru" <#{ENV.fetch('MAILER_SENDER')}>}
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.ru',
     port: 465,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
+    user_name: ENV.fetch('SMTP_USERNAME'),
+    password: ENV.fetch('SMTP_PASSWORD'),
     authentication: 'plain',
     enable_starttls_auto: true,
     tls: true
